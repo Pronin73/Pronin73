dada12d716091c383ef9b5f08cb4f391ba683d36
Pronin73 <pup2009@gmail.com>
2017-02-13 01:01:24 +0300

    Добавлено несколько функций

--- SendBox/src/main/Java/ru/prog/MyFirstProg.java
+++ SendBox/src/main/Java/ru/prog/MyFirstProg.java
@@ -3,8 +3,29 @@ package ru.prog;
 public class MyFirstProg {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");
 
+		hellow("world");
+		hellow("user");
+		hellow("Alexey");
+
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + aria(len));
+
+		double a = 4;
+		double b = 6;
+
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + aria(a, b));
+	}
+	public static void hellow ( String sambody) {
+
+		System.out.println("Hellow, " + sambody + "!");
 	}
 
+	public static double aria ( double l) {
+	return l + l;
+	}
+
+	public static double aria ( double a, double b){
+		return a * b;
+	}
 }
