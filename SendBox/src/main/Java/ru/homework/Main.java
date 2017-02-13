0a61e79df89b991ef3a04b0153147daa7963e953
Pronin73 <pup2009@gmail.com>
2017-02-13 19:24:43 +0300

    Выполнение гемморойного 4 задания с точками.
    Дергарье метода у одной точки, передача параметров второй точки.

--- SendBox/src/main/Java/ru/homework/Main.java
+++ SendBox/src/main/Java/ru/homework/Main.java
@@ -2,21 +2,22 @@ package ru.homework;
 
 public class Main {
     public static void main(String[] args) {
-        Point P1 = new Point();
-        Point P2 = new Point();
+        Point P1 = new Point(0, 1);
+        Point P2 = new Point(5, 3);
 
-        P1.x = 0;
+       /* P1.x = 0;
         P1.y = 1;
         P2.x = 5;
         P2.y = 3;
-        distance(P1, P2);
-        System.out.println( "Расстояние между двумя точками" + " = " + distance(P1, P2));
+       */
+
+        System.out.println( "Расстояние между двумя точками" + " = " + P1.distance(P2));
 
     }
 
-    public static double distance(Point P1, Point P2){
+   /* public static double distance(Point P1, Point P2){
 
         return Math.sqrt((P1.x - P2.x)*(P1.x - P2.x) + (P1.y - P2.y)*(P1.y - P2.y));
 
-    }
+    }*/
 }
--- SendBox/src/main/Java/ru/homework/Point.java
+++ SendBox/src/main/Java/ru/homework/Point.java
@@ -8,4 +8,17 @@ public class Point {
     public double x;
     public double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point P2) {
+        double d;
+        d = Math.sqrt((this.x - P2.x) * (this.x - P2.x) + (this.y - P2.y) * (this.y - P2.y));
+        return d;
+
+    }
+
 }
+
