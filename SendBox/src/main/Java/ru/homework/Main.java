2bfd8bbfe1a9fc57abab09c3d13eb1edc50e7102
Pronin73 <pup2009@gmail.com>
2017-02-13 17:28:51 +0300

    Реализация домашнего задания 1-3.

--- SendBox/src/main/Java/ru/homework/Main.java
+++ SendBox/src/main/Java/ru/homework/Main.java
@@ -5,18 +5,18 @@ public class Main {
         Point P1 = new Point();
         Point P2 = new Point();
 
-        double x1 = 0;
-        double y1 = 1;
-        double x2 = 5;
-        double y2 = 3;
-        distance( x1,  y1,  x2,  y2);
-        System.out.println( "Расстояние между двумя точками" + " = " + distance(x1, y1, x2, y2));
+        P1.x = 0;
+        P1.y = 1;
+        P2.x = 5;
+        P2.y = 3;
+        distance(P1, P2);
+        System.out.println( "Расстояние между двумя точками" + " = " + distance(P1, P2));
 
     }
 
     public static double distance(Point P1, Point P2){
-     double d = 0;
-     d = Math.sqrt((P1.x - P2.x)*(P1.x - P2.x) + (P1.y - P2.y)*(P1.y - P2.y));
-     return d;
+
+        return Math.sqrt((P1.x - P2.x)*(P1.x - P2.x) + (P1.y - P2.y)*(P1.y - P2.y));
+
     }
 }
