7dd0aa66a33dc44c76e9f3f1648eaf5e0caf60fc
Pronin73 <pup2009@gmail.com>
2017-02-20 23:26:04 +0300

    Созданы методы для формы создания контакта

--- addressbook_web_test/src/test/java/ru/addressbook/AddNewContact.java
+++ addressbook_web_test/src/test/java/ru/addressbook/AddNewContact.java
@@ -21,10 +21,10 @@ public class AddNewContact {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        loginInput();
     }
-    
-    @Test
-    public void AddNewContact() {
+
+    private void loginInput() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -33,28 +33,70 @@ public class AddNewContact {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Alexei");
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).sendKeys("\\9");
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Pronin");
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("Pronin1973");
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys("BestCompany");
+    }
+
+    @Test
+    public void NewContactadd() {
+
+
+        addNewMember();
+        addNameMember();
+        addMiddlenameMember();
+        addLastNameMember();
+        addNickName();
+        companyMember();
+        addAddressMember();
+        initMember();
+        gotoHome();
+    }
+
+    private void gotoHome() {
+        wd.findElement(By.linkText("home")).click();
+    }
+
+    private void initMember() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void addAddressMember() {
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
         wd.findElement(By.name("address")).sendKeys("VVVVVV new Moscow");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-        wd.findElement(By.linkText("home")).click();
     }
-    
+
+    private void companyMember() {
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys("BestCompany");
+    }
+
+    private void addNickName() {
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys("Pronin1973");
+    }
+
+    private void addLastNameMember() {
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys("Pronin");
+    }
+
+    private void addMiddlenameMember() {
+        wd.findElement(By.name("middlename")).sendKeys("\\9");
+        wd.findElement(By.name("lastname")).click();
+    }
+
+    private void addNameMember() {
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys("Alexei");
+        wd.findElement(By.name("middlename")).click();
+    }
+
+    private void addNewMember() {
+        wd.findElement(By.linkText("add new")).click();
+        wd.findElement(By.name("firstname")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
