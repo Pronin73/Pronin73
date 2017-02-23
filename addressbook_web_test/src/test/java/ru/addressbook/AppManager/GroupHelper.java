3aff8e1e5450a1bff0fea5a7c1a86c7f06e8703a
Pronin73 <pup2009@gmail.com>
2017-02-23 23:35:35 +0300

    Исправлена ошибка в GroupModificationTests

--- addressbook_web_test/src/test/java/ru/addressbook/AppManager/GroupHelper.java
+++ addressbook_web_test/src/test/java/ru/addressbook/AppManager/GroupHelper.java
@@ -40,7 +40,7 @@ public class GroupHelper extends HelperBase {
     }
 
     public void initGroupModification() {
-        click(By.name("edite"));
+        click(By.name("edit"));
     }
 
     public void submitGroupModification() {
--- addressbook_web_test/src/test/java/ru/addressbook/Test/GroupModificationTests.java
+++ addressbook_web_test/src/test/java/ru/addressbook/Test/GroupModificationTests.java
@@ -3,9 +3,6 @@ package ru.addressbook.Test;
 import org.testng.annotations.Test;
 import ru.addressbook.Model.GroupData;
 
-/**
- * Created by Вася on 23.02.2017.
- */
 public class GroupModificationTests extends TestBase {
 
     @Test
@@ -13,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().goToGroupePage();
         app.getGroupHelper().selectGrope();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test1"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test55", "test55", "test55"));
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().goToGroupePage();
     }
