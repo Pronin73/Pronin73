1ddda7c19d1067f7900eb44716255e6e4322ca3b
Pronin73 <pup2009@gmail.com>
2017-02-24 16:03:16 +0300

    Изменино название теста

--- addressbook_web_test/src/test/java/ru/addressbook/Test/ContactCreationTest.java
+++ addressbook_web_test/src/test/java/ru/addressbook/Test/ContactCreationTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import ru.addressbook.Model.ContactData;
 
-public class GroupContactTest {
+public class ContactCreationTest {
     FirefoxDriver wd;
     
     @BeforeMethod
