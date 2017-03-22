4f566dbbe0446cacd9ae631f973f9937e64d05bb
Pronin73 <pup2009@gmail.com>
2017-03-22 14:23:26 +0300

    Сортировка списков контактов только по названиям.

--- addressbook_new_web_test/src/test/java/addressbook/Model/ContactData.java
+++ addressbook_new_web_test/src/test/java/addressbook/Model/ContactData.java
@@ -18,7 +18,7 @@ public class ContactData {
     }
 
     public ContactData(String group, String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobile) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.group = group;
         this.firstname = firstname;
         this.middlename = middlename;
@@ -90,6 +90,24 @@ public class ContactData {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "ContactData{" +
                 "id='" + id + '\'' +
@@ -97,27 +115,6 @@ public class ContactData {
                 ", lastname='" + lastname + '\'' +
                 ", address='" + address + '\'' +
                 '}';
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ContactData that = (ContactData) o;
-
-        if (id != that.id) return false;
-        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
-        return address != null ? address.equals(that.address) : that.address == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-        result = 31 * result + (address != null ? address.hashCode() : 0);
-        return result;
     }
 
 }
--- addressbook_new_web_test/src/test/java/addressbook/Test/ContactModificationTest.java
+++ addressbook_new_web_test/src/test/java/addressbook/Test/ContactModificationTest.java
@@ -6,6 +6,7 @@ import addressbook.Test.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -32,7 +33,10 @@ public class ContactModificationTest extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(contact);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
